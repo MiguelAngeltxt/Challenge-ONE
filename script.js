9adb7cfa90d4e7e99d27dbe328b9aa0d330ac176
Miguel Falcon <angelfalconmv@gmail.com>
2024-07-28 19:29:21 -0400

    Primer Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const llaves = {
+    "e":"enter",
+    "i":"imes",
+    "a":"ai",
+    "o":"ober",
+    "u":"ufat",
+};
+
+function encriptado() {
+    var inputText = document.getElementById("textInput").value;
+    
+}
+
+console.log = llaves[encriptado()]
